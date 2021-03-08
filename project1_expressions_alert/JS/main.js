e9d68bea11df2f1299cea37bf50c5a7e7210fbea
William <will2me35@gmail.com>
2021-03-08 18:41:51 +0000

    Commiting the file for the first submission

---
+++ project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+//Setting up the alert variables
+var Hello = ("Greetings my name is ")
+var Name = ("William Twomey")
+
+//The alert window
+window.alert(Hello + Name);
+
+
+// Setting my family variables
+var Family = "The Twomeys", Dad ="John", Mam = "Emily",
+Son = "Steven";
+
+//Changing the font colour of the variables
+var Family = Family.fontcolor("red")
+var Dad = Dad.fontcolor("blue")
+var Mam = Mam.fontcolor("pink")
+var Son = Son.fontcolor("purple")
+
+document.write(Son + " is in purple");
